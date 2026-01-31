b30f8cbd1ad92f92508439aa3a5e51c948b6231f
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-01-31 13:31:20 +0700

    Fixed Logic

---
+++ entities/player/player.gd
@@ -0,0 +1,121 @@
+extends CharacterBody2D
+
+@export_category("Basic Movement")
+@export var speed: float = 150
+@export var jump_velocity: float = -270
+@export var terminal_velocity: float = 500
+@export var default_gravity: float = 980
+
+@export_category("Buffer Jump")
+@export var jump_buffer_time: float = 0.1
+
+@export_category("Coyote Time")
+@export var coyote_time: float = 0.1
+
+@export_category("Variable Jump Height")
+@export var gravity_modifier: float = 1.25
+
+@onready var buffer_timer: Timer = $BufferTimer
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var debug_label: Label = $DebugLabel
+
+# Buffer Jump & Coyote Time
+var jump_available: bool = true
+var jump_buffer: bool = false
+
+# Current Gravity
+var current_gravity: float = 0
+
+var was_on_floor: bool = false
+
+func _ready() -> void:
+	buffer_timer.wait_time = jump_buffer_time
+	coyote_timer.wait_time = coyote_time
+	current_gravity = default_gravity
+
+func _physics_process(delta: float) -> void:
+	update_debug_label()
+	handle_fall(delta)
+	handle_jump()
+	handle_movement()
+	handle_flip()
+	
+	if velocity.y > terminal_velocity:
+		velocity.y = terminal_velocity
+	
+	move_and_slide()
+	handle_anim()
+	was_on_floor = is_on_floor()
+	
+
+func handle_fall(delta: float) -> void:
+	if !is_on_floor():
+		if jump_available and coyote_timer.is_stopped():
+			coyote_timer.start()
+		
+		if velocity.y < 0 and Input.is_action_just_released("jump"): # If Falling
+			current_gravity = default_gravity * gravity_modifier
+			
+		velocity.y += current_gravity * delta
+	else:
+		jump_available = true
+		current_gravity = default_gravity
+		coyote_timer.stop()
+		if jump_buffer and !buffer_timer.is_stopped():
+			jump()
+
+func handle_jump() -> void:
+	if Input.is_action_just_pressed("jump"):
+		if jump_available:
+			jump()
+		else:
+			jump_buffer = true
+			buffer_timer.start()
+
+func handle_movement() -> void:
+	var direction := Input.get_axis("left", "right")
+	velocity.x = direction * speed
+
+func handle_anim() -> void:
+	if !was_on_floor and is_on_floor():
+		sprite.play("land")
+	
+	if sprite.is_playing() and sprite.animation == "land":
+		return
+	
+	if !is_on_floor():
+		if sprite.animation != "jump":
+			sprite.play("jump")
+		return
+	
+	if !is_zero_approx(velocity.x):
+		sprite.play("walk")
+	else:
+		sprite.play("idle")
+
+func handle_flip() -> void:
+	if !is_zero_approx(velocity.x):
+		sprite.flip_h = velocity.x <= 0
+
+func jump() -> void:
+	velocity.y = jump_velocity
+	jump_available = false
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		get_tree().paused = !get_tree().paused
+
+func _on_coyote_timer_timeout() -> void:
+	jump_available = false
+
+func update_debug_label() -> void:
+	var text: String = ""
+	text += "BUFF: %.2f\n" % buffer_timer.time_left
+	text += "COY: %.2f\n" % coyote_timer.time_left
+	text += "GRAV: %.2f," % current_gravity
+	text += "AVA: %s" % jump_available
+	debug_label.text = text
+
+func _on_buffer_timer_timeout() -> void:
+	jump_buffer = false
