13df0513d5ee0005042d53d2b9da09782c36ef3c
R. Ethan Halim <44055981+avaxar@users.noreply.github.com>
2026-02-01 10:33:38 +0700

    Add border & transition

---
+++ ui/transition/transition.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+@onready var sprite := $AnimatedSprite
+
+
+func _ready() -> void:
+    sprite.play("open")
+    Manager.on_player_death.connect(_on_player_death)
+
+
+func _on_player_death() -> void:
+    await get_tree().create_timer(0.6).timeout
+    sprite.play("close")
