4bc441d6ea072c8549f02bf166a0c005f35f084d
R. Ethan Halim <44055981+avaxar@users.noreply.github.com>
2026-02-01 16:06:00 +0700

    Names and something

--- ui/pause/pause_menu.gd
+++ ui/pause/pause_menu.gd
@@ -7,6 +7,7 @@ func _on_resume_button_pressed() -> void:
 
 func _on_restart_button_pressed() -> void:
 	get_tree().paused = false
+	visible = false
 	Manager.emit_on_player_death()
 
 func _on_menu_button_pressed() -> void:
