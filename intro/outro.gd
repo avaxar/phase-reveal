a4ca5a78c8186a7b1574ec87d329b6f5561c5b1e
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-02-01 12:35:19 +0700

    Added Outro Base

---
+++ intro/outro.gd
@@ -0,0 +1,47 @@
+extends Control
+
+@export var frames: Array[TextureRect]
+@export var duration: float = 1.0
+@export var end_wait_time: float = 2.0
+const TITLE_SCREEN = preload("uid://wncglugrhywk")
+
+
+var tween: Tween
+var current_index := 0
+
+
+func _ready():
+	for frame in frames:
+		frame.modulate.a = 0.0
+
+	play_current_frame()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		skip_current_frame()
+
+func play_current_frame():
+	if current_index > frames.size() - 1:
+		await get_tree().create_timer(end_wait_time).timeout
+		get_tree().change_scene_to_packed(TITLE_SCREEN)
+		return
+
+	var frame := frames[current_index]
+
+	tween = get_tree().create_tween()
+	tween.tween_property(frame, "modulate:a", 1.0, duration)
+	tween.finished.connect(on_frame_finished)
+
+func skip_current_frame():
+	if current_index >= frames.size():
+		return
+
+	if tween and tween.is_running():
+		tween.kill()
+		frames[current_index].modulate.a = 1.0
+
+	on_frame_finished()
+
+func on_frame_finished():
+	current_index += 1
+	play_current_frame()
