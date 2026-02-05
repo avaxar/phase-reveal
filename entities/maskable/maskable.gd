1c57fd91b964d7f49d807673c028e9f78165da3e
R. Ethan Halim <44055981+avaxar@users.noreply.github.com>
2026-02-05 23:03:48 +0700

    Implement new non-bitmap-based masking system

--- entities/maskable/maskable.gd
+++ entities/maskable/maskable.gd
@@ -1,4 +1,3 @@
-@tool
 class_name Maskable
 extends AnimatableBody2D
 
@@ -8,25 +7,9 @@ extends AnimatableBody2D
 @export var blue_mask := false
 
 
-@onready var collision_shape := $CollisionShape
 @onready var mask_area := $MaskArea
 @onready var mask_area_shape := $MaskArea/CollisionShape
 
-
-@export var disabled := false:
-	set(value):
-		disabled = value
-		if not is_inside_tree():
-			await ready
-
-		collision_shape.set_deferred("disabled", value)
-
-var size: Vector2i:
-	get:
-		if collision_shape == null:
-			return Vector2i(0, 0)
-
-		return Vector2i(collision_shape.shape.size)
 
 var _bypass_setter := false # This is so inelegant
 
@@ -37,13 +20,11 @@ var _bypass_setter := false # This is so inelegant
 			await ready
 
 		if not _bypass_setter:
-			collision_shape.position = (value + bottom_right) / 2.0
-			collision_shape.shape.size = Vector2(bottom_right - value)
-			mask_area.position = collision_shape.position
-			mask_area_shape.shape = collision_shape.shape
+			mask_area.position = (value + bottom_right) / 2.0
+			mask_area_shape.shape.size = Vector2(bottom_right - value)
 
 			_bypass_setter = true
-			bottom_right = Vector2i(collision_shape.position + collision_shape.shape.size / 2)
+			bottom_right = Vector2i(mask_area.position + mask_area_shape.shape.size / 2)
 			_bypass_setter = false
 
 @export var bottom_right := Vector2i(8, 8):
@@ -53,14 +34,28 @@ var _bypass_setter := false # This is so inelegant
 			await ready
 
 		if not _bypass_setter:
-			collision_shape.position = (top_left + value) / 2.0
-			collision_shape.shape.size = Vector2(value - top_left)
-			mask_area.position = collision_shape.position
-			mask_area_shape.shape = collision_shape.shape
+			mask_area.position = (top_left + value) / 2.0
+			mask_area_shape.shape.size = Vector2(value - top_left)
 
 			_bypass_setter = true
-			top_left = Vector2i(collision_shape.position - collision_shape.shape.size / 2)
+			top_left = Vector2i(mask_area.position - mask_area_shape.shape.size / 2)
 			_bypass_setter = false
+
+var size: Vector2i:
+	get:
+		return bottom_right - top_left
+
+var global_rect: Rect2i:
+	get:
+		return Rect2i(to_global(top_left), to_global(bottom_right) - to_global(top_left))
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+
+	Manager.mask_changed.connect(_on_mask_changed)
+	_on_mask_changed(true, true, true)
 
 
 func is_included() -> bool:
@@ -74,24 +69,145 @@ func is_included() -> bool:
 		)
 
 
-var mask_intersection_count := 0
-signal mask_joined(maskable: Maskable)
-signal mask_freed(maskable: Maskable)
+# -- Masking logic! --
+
+var intersecting: Array[Maskable]
+var divisions: Dictionary[Rect2i, int]
+var colliders: Array[CollisionShape2D]
 
 
 func _on_mask_area_area_entered(area: Area2D) -> void:
 	if area.get_parent() is not Maskable:
 		return
 
-	mask_intersection_count += 1
-	if mask_intersection_count == 1:
-		mask_joined.emit(self )
+	var maskable: Maskable = area.get_parent()
+	intersecting.append(maskable)
+
+	reset_mask(false)
 
 
 func _on_mask_area_area_exited(area: Area2D) -> void:
 	if area.get_parent() is not Maskable:
 		return
 
-	mask_intersection_count -= 1
-	if mask_intersection_count == 0:
-		mask_freed.emit(self )
+	var maskable: Maskable = area.get_parent()
+	intersecting.remove_at(intersecting.find(maskable))
+
+	reset_mask(false)
+
+
+var previous_rect := global_rect
+func _physics_process(_delta: float) -> void:
+	if global_rect == previous_rect:
+		return
+
+	reset_mask()
+	previous_rect = global_rect
+
+
+func _on_mask_changed(red_changed: bool, green_changed: bool, blue_changed: bool) -> void:
+	if (len(intersecting) > 0
+		or (red_changed and red_mask)
+		or (green_changed and green_mask)
+		or (blue_changed and blue_mask)):
+		reset_mask()
+
+
+func reset_mask(chaining := true):
+	if chaining:
+		for maskable: Maskable in intersecting:
+			maskable.reset_mask(false)
+
+	if not is_included():
+		divisions = {}
+		for collider: CollisionShape2D in colliders:
+			collider.set_deferred("disabled", true)
+		return
+
+	divisions = {global_rect: 1}
+	for maskable: Maskable in intersecting:
+		if not maskable.is_included():
+			continue
+
+		var new_divisions: Dictionary[Rect2i, int] = {}
+		for division: Rect2i in divisions:
+			var count := divisions[division]
+			var partitions := partition_rect(division, maskable.global_rect)
+			for partition: Rect2i in partitions:
+				if partitions[partition]:
+					new_divisions[partition] = count + 1
+				else:
+					new_divisions[partition] = count
+
+		divisions = new_divisions
+
+	if len(divisions) > len(colliders):
+		for i in range(len(divisions) - len(colliders)):
+			var collider := CollisionShape2D.new()
+			collider.shape = RectangleShape2D.new()
+			collider.disabled = true
+			call_deferred("add_child", collider)
+			colliders.append(collider)
+	elif len(divisions) < len(colliders):
+		for i: int in range(len(divisions), len(colliders)):
+			colliders[i].set_deferred("disabled", true)
+
+	for i: int in range(len(divisions)):
+		var division: Rect2i = divisions.keys()[i]
+		var count := divisions[division]
+		var collider := colliders[i]
+
+		if count % 2 == 1:
+			var local_division := Rect2(
+				to_local(division.position),
+				to_local(division.end) - to_local(division.position)
+			)
+
+			collider.set_deferred("disabled", false)
+			collider.position = local_division.get_center()
+			collider.shape.size = local_division.size
+		else:
+			collider.set_deferred("disabled", true)
+
+
+static func partition_rect(rect: Rect2i, mask: Rect2i) -> Dictionary[Rect2i, bool]:
+	var result: Dictionary[Rect2i, bool] = {}
+
+	# Central segment
+	var intersection := rect.intersection(mask)
+	if intersection.has_area():
+		result[intersection] = true
+	else:
+		# No overlap
+		result[rect] = false
+		return result
+
+	# Top segment
+	if intersection.position.y > rect.position.y:
+		result[Rect2i(
+			Vector2i(rect.position.x, rect.position.y),
+			Vector2i(rect.size.x, intersection.position.y - rect.position.y)
+		)] = false
+
+	# Bottom segment
+	if intersection.end.y < rect.end.y:
+		result[Rect2i(
+			Vector2i(rect.position.x, intersection.end.y),
+			Vector2i(rect.size.x, rect.end.y - intersection.end.y)
+		)] = false
+
+	# Left segment
+	if intersection.position.x > rect.position.x:
+		result[Rect2i(
+			Vector2i(rect.position.x, intersection.position.y),
+			Vector2i(intersection.position.x - rect.position.x, intersection.size.y)
+		)] = false
+
+	# Right segment
+	if intersection.end.x < rect.end.x:
+		result[Rect2i(
+			Vector2i(intersection.end.x, intersection.position.y),
+			Vector2i(rect.end.x - intersection.end.x, intersection.size.y)
+		)] = false
+
+	return result
--- level/level.gd
+++ level/level.gd
@@ -5,13 +5,14 @@ extends Node2D
 @export var default_green := false
 @export var default_blue := false
 
-@onready var collision_proxies := $CollisionProxies
 @onready var passthrough_visualizer := $PassthroughVisualizer
 @onready var player := $Player
 @onready var camera := $Player/Camera
 @onready var end := $End
 @onready var hud := $CanvasLayer/HUD
 @onready var transition := $CanvasLayer/Transition
+
+var passthrough_meshes: Array[MeshInstance2D]
 
 
 func _ready():
@@ -21,21 +22,6 @@ func _ready():
 	Manager.blue_mask = default_blue
 
 	camera.limit_right = end.position.x
-	Manager.mask_changed.connect(update_masks)
-	for maskable: Maskable in get_tree().get_nodes_in_group("maskables"):
-		maskable.mask_joined.connect(_on_mask_joined)
-		maskable.mask_freed.connect(_on_mask_freed)
-
-
-func _process(_delta: float) -> void:
-	for maskable: Maskable in get_tree().get_nodes_in_group("maskables"):
-		if maskable in maskable_rects:
-			continue
-
-		maskable.disabled = not maskable.is_included()
-
-	watch_masks()
-	regenerate_chunks()
 
 
 func _on_end_body_entered(body: Node2D) -> void:
@@ -46,155 +32,35 @@ func _on_end_body_entered(body: Node2D) -> void:
 		Manager.load_next_level()
 
 
-const CHUNK_SIZE := 32
-var dirty_chunks: Dictionary[Vector2i, bool]
-var addition_chunks: Dictionary[Vector2i, PackedByteArray]
-var bitmask_chunks: Dictionary[Vector2i, BitMap]
-var passthrough_chunks: Dictionary[Vector2i, BitMap]
-var chunk_bodies: Dictionary[Vector2i, StaticBody2D]
-var passthrough_polygons: Dictionary[Vector2i, Node2D]
-var maskable_rects: Dictionary[Maskable, Rect2i]
-var maskable_drawn: Dictionary[Maskable, bool]
+func _process(_delta: float) -> void:
+	draw_visualizer()
 
 
-func _on_mask_joined(maskable: Maskable) -> void:
-	print("Joined: ", maskable)
-	maskable.disabled = true
-	maskable_rects[maskable] = Rect2i(maskable.to_global(maskable.top_left), maskable.size)
-	maskable_drawn[maskable] = false
-	update_mask(maskable)
+func draw_visualizer() -> void:
+	# Uses a makeshift set to get rid of duplicates
+	var masked_rects: Dictionary[Rect2i, bool] = {}
+	for maskable: Maskable in get_tree().get_nodes_in_group("maskables"):
+		for division: Rect2i in maskable.divisions:
+			var count := maskable.divisions[division]
+			if count != 0 and count % 2 == 0:
+				masked_rects[division] = true
 
+	if len(masked_rects) > len(passthrough_meshes):
+		for i in range(len(masked_rects) - len(passthrough_meshes)):
+			var mesh := MeshInstance2D.new()
+			mesh.mesh = QuadMesh.new()
+			mesh.visible = false
+			mesh.use_parent_material = true
+			passthrough_visualizer.add_child(mesh)
+			passthrough_meshes.append(mesh)
+	elif len(masked_rects) < len(passthrough_meshes):
+		for i: int in range(len(masked_rects), len(passthrough_meshes)):
+			passthrough_meshes[i].visible = false
 
-func _on_mask_freed(maskable: Maskable) -> void:
-	print("Left: ", maskable)
-	maskable.disabled = not maskable.is_included()
-	update_mask(maskable, true)
-	if maskable in maskable_rects:
-		maskable_rects.erase(maskable)
+	for i: int in len(masked_rects):
+		var rect: Rect2i = masked_rects.keys()[i]
+		var mesh := passthrough_meshes[i]
 
-
-func watch_masks() -> void:
-	for maskable: Maskable in maskable_rects:
-		var old_rect := maskable_rects[maskable]
-		var rect := Rect2i(maskable.to_global(maskable.top_left), maskable.size)
-
-		if old_rect != rect:
-			if maskable_drawn[maskable]:
-				mask_rect(old_rect, -1)
-				mask_rect(rect, 1)
-			maskable_rects[maskable] = rect
-
-
-func update_masks(red_changed: bool, green_changed: bool, blue_changed: bool) -> void:
-	for maskable: Maskable in maskable_rects:
-		if ((red_changed and maskable.red_mask)
-			or (green_changed and maskable.green_mask)
-			or (blue_changed and maskable.blue_mask)):
-			update_mask(maskable)
-
-
-func update_mask(maskable: Maskable, remove := false) -> void:
-	if maskable not in maskable_drawn:
-		return
-
-	if (not maskable.is_included() or remove) and maskable_drawn[maskable]:
-		if maskable in maskable_rects:
-			mask_rect(maskable_rects[maskable], -1)
-
-		maskable_drawn[maskable] = false
-	elif maskable.is_included() and not maskable_drawn[maskable]:
-		mask_rect(maskable_rects[maskable], 1)
-		maskable_drawn[maskable] = true
-
-
-func mask_rect(rect: Rect2i, delta: int) -> void:
-	var tl_chunk := Vector2i((rect.position / float(CHUNK_SIZE)).floor())
-	var br_chunk := Vector2i((rect.end / float(CHUNK_SIZE)).ceil())
-
-	for chunk_y: int in range(tl_chunk.y, br_chunk.y):
-		var start_y := 0 if chunk_y != tl_chunk.y else posmod(rect.position.y, CHUNK_SIZE)
-		var end_y := CHUNK_SIZE if chunk_y != br_chunk.y - 1 \
-			else (CHUNK_SIZE if posmod(rect.end.y, CHUNK_SIZE) == 0 else posmod(rect.end.y, CHUNK_SIZE))
-
-		for chunk_x: int in range(tl_chunk.x, br_chunk.x):
-			var chunk := Vector2i(chunk_x, chunk_y)
-			dirty_chunks[chunk] = true
-
-			var addition_chunk: PackedByteArray
-			if chunk in addition_chunks:
-				addition_chunk = addition_chunks[chunk]
-			else:
-				addition_chunk = PackedByteArray()
-				addition_chunk.resize(CHUNK_SIZE * CHUNK_SIZE)
-				addition_chunk.fill(0)
-				addition_chunks[chunk] = addition_chunk
-
-			var bitmask_chunk: BitMap
-			if chunk in bitmask_chunks:
-				bitmask_chunk = bitmask_chunks[chunk]
-			else:
-				bitmask_chunk = BitMap.new()
-				bitmask_chunk.create(Vector2i(CHUNK_SIZE, CHUNK_SIZE))
-				bitmask_chunks[chunk] = bitmask_chunk
-
-			var passthrough_chunk: BitMap
-			if chunk in passthrough_chunks:
-				passthrough_chunk = passthrough_chunks[chunk]
-			else:
-				passthrough_chunk = BitMap.new()
-				passthrough_chunk.create(Vector2i(CHUNK_SIZE, CHUNK_SIZE))
-				passthrough_chunks[chunk] = passthrough_chunk
-
-			var start_x := 0 if chunk_x != tl_chunk.x else posmod(rect.position.x, CHUNK_SIZE)
-			var end_x := CHUNK_SIZE if chunk_x != br_chunk.x - 1 \
-				else (CHUNK_SIZE if posmod(rect.end.x, CHUNK_SIZE) == 0 else posmod(rect.end.x, CHUNK_SIZE))
-
-			for y in range(start_y, end_y):
-				for x in range(start_x, end_x):
-					var cumulative := addition_chunk[y * CHUNK_SIZE + x] + delta
-					addition_chunk[y * CHUNK_SIZE + x] = cumulative
-					bitmask_chunk.set_bit(x, y, cumulative % 2 == 1) # Solid when odd
-					passthrough_chunk.set_bit(x, y, (cumulative % 2 == 0) and cumulative != 0)
-
-
-func regenerate_chunks() -> void:
-	for chunk: Vector2i in dirty_chunks:
-		var chunk_body: StaticBody2D
-		if chunk in chunk_bodies:
-			chunk_body = chunk_bodies[chunk]
-		else:
-			chunk_body = StaticBody2D.new()
-			collision_proxies.add_child(chunk_body)
-			chunk_body.global_position = Vector2(chunk) * CHUNK_SIZE
-			chunk_bodies[chunk] = chunk_body
-
-		for child: Node in chunk_body.get_children():
-			child.queue_free()
-
-		for polygon: PackedVector2Array in bitmask_chunks[chunk].opaque_to_polygons(
-				Rect2i(Vector2i(0, 0), Vector2i(CHUNK_SIZE, CHUNK_SIZE)), 0.0):
-			var poly := CollisionPolygon2D.new()
-			poly.polygon = polygon
-			chunk_body.add_child(poly)
-
-		var passthrough_polygon: Node2D
-		if chunk in passthrough_polygons:
-			passthrough_polygon = passthrough_polygons[chunk]
-		else:
-			passthrough_polygon = Node2D.new()
-			passthrough_visualizer.add_child(passthrough_polygon)
-			passthrough_polygon.global_position = Vector2(chunk) * CHUNK_SIZE
-			passthrough_polygon.use_parent_material = true
-			passthrough_polygons[chunk] = passthrough_polygon
-
-		for child: Node in passthrough_polygon.get_children():
-			child.queue_free()
-
-		for polygon: PackedVector2Array in passthrough_chunks[chunk].opaque_to_polygons(
-				Rect2i(Vector2i(0, 0), Vector2i(CHUNK_SIZE, CHUNK_SIZE)), 0.0):
-			var poly := Polygon2D.new()
-			poly.polygon = polygon
-			poly.use_parent_material = true
-			passthrough_polygon.add_child(poly)
-
-	dirty_chunks = {}
+		mesh.visible = true
+		mesh.position = Rect2(rect).get_center()
+		mesh.mesh.size = rect.size
