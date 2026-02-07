f0ebf065083ca989cd8682a49293d2fe82bd8bd0
Troggz <thomasnadandraz@gmail.com>
2026-02-07 22:57:39 +0700

    Some level balancing/polishing
    level 10
    level 5
    level 6
    level 8

--- ui/transition/transition.gd
+++ ui/transition/transition.gd
@@ -5,15 +5,15 @@ extends Control
 
 
 func _ready() -> void:
-    sprite.play("open")
-    Manager.on_player_death.connect(_on_player_death)
+	sprite.play("open")
+	Manager.on_player_death.connect(_on_player_death)
 
 
 func _on_player_death() -> void:
-    await get_tree().create_timer(0.6).timeout
-    sprite.play("close")
+	await get_tree().create_timer(0.6).timeout
+	sprite.play("close")
 
 
 func close() -> void:
-    sprite.play("close")
-    await get_tree().create_timer(0.4).timeout
+	sprite.play("close")
+	await get_tree().create_timer(0.4).timeout
